c31bf0d3a27dbd6717cff803370cbe4aa6ce54d0
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-19 18:35:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import openai
 st.set_page_config(page_title="Harish's Bot", page_icon=" 🤖📚", layout="wide", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets["my_cool_secrets"]["openai_key"]
 st.title("🌟 Ask anything about Harish....")
-
+st.write(openai.api_key)
 st.info("Check Out my Complete Portfolio [here](https://harishdhanarajan.streamlit.app/) !", icon="📃")
 
 #Initiate the Session and Create a temp memory
@@ -17,16 +17,7 @@ if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
     ]
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        return index
 
-index = load_data()
 
 #Initiate Chat Engine
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
