b626271617512710dd9fb6896f48669662152000
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-19 18:14:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-import os
+from llama_index.llms.openai import OpenAI
+try:
+  from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+except ImportError:
+  from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+import openai
 
 st.write(st.secrets["my_cool_secrets"]["openai_key"])
 
