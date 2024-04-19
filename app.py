53df31cb7391565a6d02adfeb0c69036de66fb69
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-19 18:15:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ except ImportError:
   from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 import openai
 
-st.write(st.secrets["my_cool_secrets"]["openai_key"])
+st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets["my_cool_secrets"]["openai_key"]
+
+st.write(openai.api_key)
 
 #[my_cool_secrets]
 #openai_key = "sk-proj-tMubK0K5nswmWfUCDMjhT3BlbkFJmvMJhM5PiUk8Gnj8F6CI"
