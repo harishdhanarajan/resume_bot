38a61ee6d918c3af1ae9b3918e01289902890931
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-20 00:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import time
 import streamlit as st
 from llama_index.llms.openai import OpenAI
 try:
@@ -30,7 +29,7 @@ def load_data():
 index = load_data()
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="context", verbose=True)
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -38,18 +37,12 @@ if prompt := st.chat_input("Your question"): # Prompt for user input and save to
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
-      
 
-  
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
-            def stream_data():
-              for word in response.response.split(" "):
-                yield word + " "
-                time.sleep(0.05)
-            st.write_stream(stream_data)
-            message = {"role": "assistant", "content": stream_data}
-            st.session_state.messages.append(message) # Add response to message history
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
