5bcf97e70f8952bc2b0209b904459f97a89e1d3b
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-19 17:58:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import os
+
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["openai_key"])
+
+#[my_cool_secrets]
+#openai_key = "sk-proj-tMubK0K5nswmWfUCDMjhT3BlbkFJmvMJhM5PiUk8Gnj8F6CI"
