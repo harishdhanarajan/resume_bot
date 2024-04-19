327de90556029d2ebff93cc23044bf63e0177321
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-20 00:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ for message in st.session_state.messages: # Display the prior chat messages
         st.write(message["content"])
       
 def stream_data():
-  for word in words.split(" "):
+  for word in response.response.split(" "):
     yield word + " "
     time.sleep(0.05)
   
@@ -49,7 +49,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
-            words = response.response
+            #response.response
             st.write_stream(stream_data)
             message = {"role": "assistant", "content": stream_data}
             st.session_state.messages.append(message) # Add response to message history
