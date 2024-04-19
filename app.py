081e32ad09a488f3c9231f3e040a453b9a03a147
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-20 01:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,23 @@ import openai
 
 st.set_page_config(page_title="Harish's Bot", page_icon=" 🤖📚", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("🌟 Ask anything about Harish....")
+gradient_text_html = """
+<style>
+.gradient-text {
+    font-weight: bold;
+    background: -webkit-linear-gradient(left, red, orange);
+    background: linear-gradient(to right, red, orange);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    display: inline;
+    font-size: 3em;
+}
+</style>
+<div class="gradient-text">Ask Me About Harish....</div>
+"""
+
+st.markdown(gradient_text_html, unsafe_allow_html=True)
+
 st.info("Check Out my Complete Portfolio [here](https://harishdhanarajan.streamlit.app/) !", icon="📃")
 
 #Initiate the Session and Create a temp memory
