82d0d660246c72030a020ccfc8abe898323d7111
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-19 18:22:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,11 @@ st.title("🌟 Ask anything about Harish....")
 
 st.info("Check Out my Complete Portfolio [here](https://harishdhanarajan.streamlit.app/) !", icon="📃")
 
+#Initiate the Session and Create a temp memory
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+    ]
+  
 #[my_cool_secrets]
 #openai_key = "sk-proj-tMubK0K5nswmWfUCDMjhT3BlbkFJmvMJhM5PiUk8Gnj8F6CI"
