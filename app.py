3a5a6c23c98e973ed337fdbd143df5a858d7e1d1
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-20 08:36:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,15 +66,3 @@ if st.session_state.messages[-1]["role"] != "assistant":
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
