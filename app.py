d54cf3e743eb98b3cc287d1437c991093fafe2b3
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-20 08:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,10 +64,18 @@ if st.session_state.messages[-1]["role"] != "assistant":
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message)
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+footer {
+	
+	visibility: hidden;
+	
+	}
+footer:after {
+	content:'goodbye'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
+}
