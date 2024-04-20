f40c3cde99eb9d640042f594edd6c14d789c5669
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-20 08:50:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ except ImportError:
 import openai
 
 
-st.set_page_config(page_title="Harish's Bot", page_icon=" 🤖📚", layout="centered",menu_items={"hide_menu": True})
+st.set_page_config(page_title="Harish's Bot", page_icon=" 🤖📚", layout="wide",menu_items={"hide_menu": True})
 
 openai.api_key = st.secrets.openai_key
 gradient_text_html = """
